6cbaf57646a981e79c8d21d33e6bb3861b77bbaa
Jhonatan C <j_cachi@outlook.com>
2021-04-04 12:48:30 -0500

    Add files via upload
    Challenge Tranzact v1

---
+++ Wikimedia/Application/Application.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wikimedia.Domain;
+using Wikimedia.Infraestructure;
+
+namespace Wikimedia.Application
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            List<Date> dates = new List<Date>();
+            List<Page> pages = new List<Page>();
+            PageDomain wikimedia = new PageDomain();
+            dates = wikimedia.ObtainDates();
+            wikimedia.GetFiles(dates);
+            wikimedia.DecompressAll();
+            wikimedia.ObtainAllMaxViewPerPage(dates);
+            wikimedia.ObtainAllMaxViewLD(dates);
+        }
+    }
+}
---
+++ Wikimedia/Domain/PageEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wikimedia.Domain
+{
+    class PageEntity
+    {
+        public String domainCode { get; set; }
+        public String pageTitle {get; set;}
+        public int viewCount { get; set; }
+        
+    }
+}
