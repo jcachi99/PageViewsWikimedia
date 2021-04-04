826809aa60e6502c2e15733cf644e8c05950822d
Jhonatan C <j_cachi@outlook.com>
2021-04-04 13:46:45 -0500

    Add files via upload
    PageViewsWikimedia Challenge v2

---
+++ Wikimedia/Infraestructure/DateEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wikimedia.Domain
+{
+    class DateEntity
+    {
+
+        public String year { get; set; }
+        public String month { get; set; }
+        public String day { get; set; }
+        public String hour { get; set; }
+
+        public DateEntity(string year, string month, string day, string hour)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.hour = hour;
+        }
+    }
+}
---
+++ Wikimedia/Infraestructure/PageEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Wikimedia.Infraestructure
+{
+    class PageView
+    {
+        public String period { get; set; }
+        public String language { get; set; }
+        public String domain { get; set;}
+        public String pageTitle { get; set; }
+        public int viewCount { get; set; }
+
+        public PageView(string period, string language, string domain, string pageTitle, int viewCount)
+        {
+            this.period = period;
+            this.language = language;
+            this.domain = domain;
+            this.pageTitle = pageTitle;
+            this.viewCount = viewCount;
+        }
+    }
+}
